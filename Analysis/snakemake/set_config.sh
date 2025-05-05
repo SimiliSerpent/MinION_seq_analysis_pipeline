51162b441383f863cdcb6f0ab3d49fa622e16887
SimiliSerpent <bvacus@cnrgh.fr>
2025-05-05 14:45:15 +0200

    Re-ran analysis on EXP250327 to include GA-repeats filtering.

--- Analysis/snakemake/set_config.sh
+++ Analysis/snakemake/set_config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export EXP_ID="EXP250417_1"
+export EXP_ID="EXP250327_1"
 export NTHREADS=32
-export SPECIES="mouse,SARSCoV2"
+export SPECIES="human,mouse"
 
 mkdir -p ../${EXP_ID}/logs
